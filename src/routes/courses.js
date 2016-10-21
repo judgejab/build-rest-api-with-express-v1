0fc53a38a37adada6e847e4db5f7e4a53691f659
judgejab <mattcleary@gmail.com>
2016-10-21 18:16:22 +0100

    Fixed initial get route for courses

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -7,8 +7,21 @@ var router = express.Router();
 
 // Returns the Course "_id" and "title" properties
 router.get('/courses', function(req, res, next){
-	res.send(JSON.stringify([{_id: 1000}, {name: "something"}]))
+	res.send(
+		JSON.stringify({
+    data: [
+      {
+        _id: 1,
+        user: {
+          _id: 1,
+          fullName: 'Joe Smith'
+        },
+        title: "something"
+    }]}
+
+    ));
 });
+
 
 // Returns all Course properties and related documents for the provided course ID
 router.get('/course/:id', function(req, res, next){
