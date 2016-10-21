3d990fa9b351b6b7b62745a78efa931481781126
judgejab <mattcleary@gmail.com>
2016-10-21 17:21:09 +0100

    trying to get route to work for courses

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -5,10 +5,10 @@ var express = require('express');
 //create router 
 var router = express.Router();
 
-// // Returns the Course "_id" and "title" properties
-// router.get('/courses', function(req, res, next){
-// 	//res.json('data/data.json', [{title:}, {_id:}]);
-// });
+// Returns the Course "_id" and "title" properties
+router.get('/courses', function(req, res, next){
+	return [{title: "foo"},{_id: "bar"}];
+});
 
 // Returns all Course properties and related documents for the provided course ID
 router.get('/course/:id', function(req, res, next){
