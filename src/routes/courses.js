d1e32a0856ac625f82a3dbe0a5b2af9584138d3f
judgejab <mattcleary@gmail.com>
2016-10-21 14:38:10 +0100

    added read

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -5,23 +5,23 @@ var express = require('express');
 //create router 
 var router = express.Router();
 
-// Returns the Course "_id" and "title" properties
-router.get('/api/courses', function(req, res, next){
-	//res.json('data/data.json', [{title:}, {_id:}]);
-});
+// // Returns the Course "_id" and "title" properties
+// router.get('/courses', function(req, res, next){
+// 	//res.json('data/data.json', [{title:}, {_id:}]);
+// });
 
 // Returns all Course properties and related documents for the provided course ID
-router.get('/api/course/:id', function(req, res, next){
+router.get('/course/:id', function(req, res, next){
 
 });
 
 //Creates a course, sets the Location header, and returns no content
-router.post('/api/courses', function(req, res, next){
+router.post('/courses', function(req, res, next){
 
 });
 
 //Updates a course and returns no content
-router.put('/api/courses/:id', function(req, res, next){
+router.put('/courses/:id', function(req, res, next){
 
 });
 
